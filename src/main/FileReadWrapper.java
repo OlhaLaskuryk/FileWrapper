fa28d45278a0bddd68e9ae1f13552be30d5f4860
Olha Laskuryk <Olha_Laskuryk@epam.com>
2015-03-23 16:59:03 +0200

    first commit

---
+++ src/main/FileReadWrapper.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author Olha_Laskuryk
+ * 
+ */
+public class FileReadWrapper {
+	private String filename;
+
+	/**
+	 * @param filename
+	 */
+	public FileReadWrapper(String filename) {
+		super();
+		this.filename = filename;
+	}
+
+}
